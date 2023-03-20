10796a6f1a1fe48e7bf5b6a6f9606850bf13c461
Kevin Reid <kevinnivek@me.com>
2023-03-20 16:16:13 -0400

    added react bootstrap and navbars

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/"> Amazonia</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazonia</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <Link to="/"> Amazonia</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route
-              path="/product/:slug"
-              element={<ProductScreen></ProductScreen>}
-            ></Route>
-            <Route path="/" element={<HomeScreen></HomeScreen>} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route
+                path="/product/:slug"
+                element={<ProductScreen></ProductScreen>}
+              ></Route>
+              <Route path="/" element={<HomeScreen></HomeScreen>} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All Rights Reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
